c1374ba5a6b84d8bca7433ccea10d956ccb1666f
Enda Quigley <endaquigley@gmail.com>
2023-04-25 23:39:59 +0100

    add immer

--- src/hooks/useStore.tsx
+++ src/hooks/useStore.tsx
@@ -1,56 +1,61 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import { v4 as uuidv4 } from "uuid";
 
 import { Item, ItemStatus } from "../types";
 
-interface StoreState {
+type State = {
   items: Item[];
+};
+
+type Actions = {
   addItem: (title: string) => void;
   removeItem: (id: string) => void;
   toggleItemStatus: (id: string) => void;
-}
+};
 
-const useStore = create<StoreState, [["zustand/persist", StoreState]]>(
+const createItem = (title = ""): Item => ({
+  id: uuidv4(),
+  title: title.trim(),
+  status: ItemStatus.Incomplete,
+  dateCreated: new Date(),
+});
+
+const toggleItemStatus = (status: ItemStatus): ItemStatus => {
+  return status === ItemStatus.Incomplete
+    ? ItemStatus.Done
+    : ItemStatus.Incomplete;
+};
+
+const useStore = create(
   persist(
-    (set) => ({
+    immer<State & Actions>((set) => ({
       items: [],
       addItem: (title = "") => {
-        return set((state) => ({
-          items: [
-            ...state.items,
-            {
-              id: uuidv4(),
-              title: title.trim(),
-              status: ItemStatus.Incomplete,
-              dateCreated: new Date(),
-            },
-          ],
-        }));
+        return set((state) => {
+          state.items.push(createItem(title));
+        });
       },
       toggleItemStatus: (id: string) => {
-        return set((state) => ({
-          items: state.items.map((item) => {
-            if (item.id === id) {
-              return {
-                ...item,
-                status:
-                  item.status === ItemStatus.Incomplete
-                    ? ItemStatus.Done
-                    : ItemStatus.Incomplete,
-              };
-            } else {
-              return item;
-            }
-          }),
-        }));
+        return set((state) => {
+          const item = state.items.find((item) => {
+            return item.id === id;
+          });
+
+          if (!item) return;
+
+          item.status = toggleItemStatus(item.status);
+        });
       },
       removeItem: (id: string) => {
         return set((state) => ({
-          items: state.items.filter((item) => item.id !== id),
+          items: state.items.filter((item) => {
+            return item.id !== id;
+          }),
         }));
       },
-    }),
+    })),
     {
       name: "todo-store",
     }
